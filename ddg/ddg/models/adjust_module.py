bdde4b216aae069168754b1317afb95ac0ee3118
sowonny <choiori97@gmail.com>
2024-02-20 17:17:20 +0900

    add: KF code

---
+++ ddg/ddg/models/adjust_module.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class KF(nn.Module):
+    def __init__(self,x, ):
+        super(KF, self).__init__()
+        
+        self.curr_x=
+        self.pred_x=
+        self.curr_cov=torch.cov(x)
+        self.pred_cov=
+        self.normal_cov=
+        self.noise_cov=
+        self.pred_matrix
+        
+    def __forward__(self, x):
+
+
+        return 
+
+
+
